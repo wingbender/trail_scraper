1b3dd05f50388009382f5f7c3570256f07d85b48
sagivy <sagiv.yaari@gmail.com>
2019-11-28 18:02:57 +0200

    migrated to mysql, work in progress

---
+++ create_trails_db.sql
@@ -0,0 +1,43 @@
+DROP DATABASE IF EXISTS trails;
+CREATE DATABASE trails;
+USE trails;
+
+DROP TABLE IF EXISTS `trails`;
+CREATE TABLE `trails` (
+  `trail_id` int PRIMARY KEY,
+  `wikiloc_id` INT,
+  `title` TEXT,
+  `url` TEXT,
+  `user_id` INT,
+  `category_id` INT,
+  `country` TEXT,
+  `distance` FLOAT,
+  `loop` BOOL,
+  `elevation_gain` FLOAT,
+  `elevation_loss` FLOAT,
+  `elevation_max` FLOAT,
+  `elevation_min` FLOAT,
+  `moving_time` INT,
+  `total_time` INT,
+  `uploaded` TEXT,
+  `recorded` TEXT,
+  `coords_n` INT,
+  `difficulty` INT
+);
+
+DROP TABLE IF EXISTS `categories`;
+CREATE TABLE `categories` (
+  `category_id` int PRIMARY KEY,
+  `category_name` TEXT
+);
+
+CREATE TABLE `users` (
+  `user_id` int PRIMARY KEY,
+  `user_name` TEXT
+);
+
+ALTER TABLE `categories` ADD FOREIGN KEY (`category_id`) REFERENCES `trails` (`category_id`);
+
+ALTER TABLE `users` ADD FOREIGN KEY (`user_id`) REFERENCES `trails` (`user_id`);
+
+CREATE INDEX `trail_ids_idx` ON `trails` (`trail_id`, `wikiloc_id`);
